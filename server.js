d365173f494725e05ccd00918c40dec351fc6518
Chris <chrisramirez15@yahoo.com>
2022-01-30 04:06:34 -0600

    connected back-end

--- server.js
+++ server.js
@@ -1,18 +1,60 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 //Async process
 const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 //server set up
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //middleware
 app.use(express.static("./develop/public"));
+
+
+// API GET request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+//API POST request
+app.post("/api/notes", function(req, res) {
+    const note = req.body
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes
+    }).then (function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+});
+
+//HTML routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+//PORT listen
+app.listen(PORT, function() {
+    console.log("Listening port is " + PORT);
+});
